919687bae78cb6ba1bd728eaa488adea3ce63768
pshankinclarke <38367844+pshankinclarke@users.noreply.github.com>
2018-06-14 01:40:40 -0700

    Add files via upload

--- GLVGeneralized.py
+++ GLVGeneralized.py
@@ -6,11 +6,11 @@ Created on Sun Jun 10 19:13:32 2018
 @author: parkershankin-clarke
 """
 
-#import barebones_CDI as bb
-#import numpy as np
-#from sympy.abc import x
-#from sympy.matrices import Matrix, Transpose
-#from numpy.linalg import inv
+import barebones_CDI as bb
+import numpy as np
+from sympy.abc import x
+from sympy.matrices import Matrix, Transpose
+from numpy.linalg import inv
 #
 ## import data from stein_ic.csv and stein_parameters.csv files
 #var_data, ic_data = bb.import_data()
@@ -28,22 +28,20 @@ Created on Sun Jun 10 19:13:32 2018
 #  I am making up a test case with a 4x4 M matrix and 4X1 vector to use. 
 
 
-#print(labels)
-#print(M)
-#print(ic4)
-#print(mu)
-import itertools
-from sympy.matrices import Matrix, Transpose
-import numpy as np
-from numpy import append
+##print(labels)
+##print(M)
+##print(ic4)
+##print(mu)
+#import itertools
+#from sympy.matrices import Matrix, Transpose
+#import numpy as np
+#from numpy import append
 
 #intializations :
 
 #These are test M and mu matrices and vectors respectively :
-M = [[3, 4, 7, 8], [4,5,6,9], [3,6,7, 10] ,[3,8,8, 10]]
+M = [[3, 4, 7, 8], [4,5,6,9], [3,6,7,10] ,[3,8,8, 10]]
 mu = [1,2,3,4]
-free = [[3, 4, 7, 8], [4,5,6,9], [3,6,7, 10] ,[3,8,8, 10]]
-free_reinitizier = [[3, 4, 7], [4,5,6], [3,6,7]]
 bbb = [1,2,3,4]
 
 #This is going to generate a list of numbers that will signify the positions of rows in my vector/ matrix:
@@ -56,6 +54,7 @@ perm_set = []
 ds_perm_set = []
 glst = []
 mu1_lst = []
+mu2_lst = []
 M1_lst = []
 M2_lst = []
 listarray = []
@@ -84,8 +83,8 @@ for j in range(len(perm_set_1)):
     perm_set_1_ds = perm_set_1[j]
     perm_set_1_ds_ds = perm_set_1_ds[0]
     M[(perm_set_1_ds_ds)-1] = [0] * len(M)
-    bbb[(perm_set_1_ds_ds)-1] = [0] * 1
-    mu1_lst = bbb + mu1_lst
+    mu[(perm_set_1_ds_ds)-1] = 0 * 1
+    mu1_lst = mu + mu1_lst
     M1_lst = M + M1_lst 
 
 
@@ -94,35 +93,55 @@ for j in range(len(perm_set_1)):
 perm_set_g1=combs[1:len(combs)-1]
 for k in range(len(perm_set_g1)):
     M = [[3, 4, 7, 8], [4,5,6,9], [3,6,7, 10] ,[3,8,8, 10]]
+    mu = [1,2,3,4]
     perm_set_g1_ds = perm_set_g1[k]
     for l in range(len(perm_set_g1_ds)):
         perm_set_g1_ds_ds = perm_set_g1_ds[l]
         M = [[3, 4, 7, 8], [4,5,6,9], [3,6,7, 10] ,[3,8,8, 10]]
+        mu = [1,2,3,4]
         for m in range (len(perm_set_g1_ds_ds)):
             perm_set_g1_ds_ds_ds = perm_set_g1_ds_ds[m]
             M[(perm_set_g1_ds_ds_ds)-1] = [0] * len(M)
+            mu[(perm_set_g1_ds_ds_ds)-1] = 0 * 1
             M2_lst = M + M2_lst   
+            mu2_lst = mu + mu2_lst 
 
 #Collects all generated matrices and stores them in a list
-fr = []
-for n in range(0,len(glst),4):
-        fr = [(glst[n:n+4])] + fr
+le = []
+for n in range(0,len(M2_lst),len(M)):
+        le = [(M2_lst[n:n+len(M)])] + le
+lem = [] 
+for n in range(0,len(M1_lst),len(M)):
+    lem = [(M1_lst[n:n+len(M)])] + lem
 
-# Converts the matricies data types to np.array in order for fixed point analysis.        
-fr_a = np.array(fr)
+lemo = []
+for o in range(0,len(mu1_lst),len(mu)):
+    lemo = [(mu1_lst[n:n+len(mu)])] + lemo
 
-
+lemon = []
+for p in range(0,len(mu2_lst),len(mu)):
+    lemon = [(mu2_lst[n:n+len(mu)])] + lemon
+    
+    
+# Converts the matricies data types to np.array in order for fixed point analysis.  
+# Note that it is important that k and kiwi //  ki and kiw have the same lengths      
+k = np.array(le)
+ki = np.array(lem)
+kiw = np.array(lemo)
+kiwi = np.array(lemon)
 
 
 #Eventually we will plug the above lists in to this part of the code and it will solve for fixed points
 # Find fixed points by solving x = A^(-1) b
-#A = M
-#b = mu
-#IA = inv(A)
-#tb=np.transpose(b)
-#x = IA.dot(b)
 
-#This part of the code will solve for the solution where you have no rows that are zero
+
+for q in range(len(k)):
+    A = k[q]
+    b = kiwi[q]
+    IA = inv(A)
+#    x = IA.dot(b)
+
+
 #A = M
 #b = mu
 #IA = inv(A)
