fdca2388247660df364015bd0511e46c33b0bd25
pshankinclarke <38367844+pshankinclarke@users.noreply.github.com>
2018-08-19 21:52:32 -0700

    Add files via upload

---
+++ GUIMicrobe.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 20:06:38 2018
+
+@author: parkershankin-clarke
+"""
+import sys
+from tkinter import *
+from tkinter import Tk
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from PIL import *
+
+
+def getfile():
+    """ This function when called opens a file """
+    filename = askopenfilename() 
+    print(filename)
+    
+def displayimage():
+    """ This function when called displays an image"""
+    #https://stackoverflow.com/questions/15999661/image-in-tkinter-window-by-clicking-on-button
+    novi = Toplevel()
+    canvas = Canvas(novi, width = 300, height = 200)
+    canvas.pack(expand = YES, fill = BOTH)
+    gif1 = PhotoImage(file = 'Figure.png')
+    #image not visual
+    canvas.create_image(50, 10, image = gif1, anchor = NW)
+    #assigned the gif1 to the canvas object
+    canvas.gif1 = gif1
+
+root = tk.Tk()
+
+
+button1 = tk.Button(root,text ='Display attractor network',command = displayimage, height=5, width=20) 
+button = tk.Button(root, text="Choose file", command=getfile)
+
+button.pack()
+button1.pack()
+
+root.mainloop()
+  
